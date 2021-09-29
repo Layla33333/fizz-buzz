df001cb481ea24502767fe1c285d58402b62edd9
layla <lkesssler3@cnm.edu>
2021-09-29 15:19:43 -0600

    Use if else if ladder

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -2,22 +2,25 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
+  static final String FIZZ_BUZZ_VALUE = "FizzBuzz";
+  static final String FIZZ_VALUE = "Fizz";
+  static final String BUZZ_VALUE = "Buzz";
+
   public String getFizzBuzz(int value) {
     String str;
 
     if (value % 3 == 0) {
       if (value % 5 == 0) {
-        str = "FizzBuzz";
+        str = FIZZ_BUZZ_VALUE;
       } else {
-        str = "Fizz";
+        str = FIZZ_VALUE;
       }
 
-    } else
-      if (value % 5 == 0) {
-        str = "Buzz";
-      } else {
-        str = String.valueOf(value);
-      }
+    } else if (value % 5 == 0) {
+      str = BUZZ_VALUE;
+    } else {
+      str = String.valueOf(value);
+    }
 
     return str;
 
